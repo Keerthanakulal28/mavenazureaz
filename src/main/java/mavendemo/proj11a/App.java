18ae564d9768413e27ae4ed8122856162b5e5dfd
Keerthanakulal28 <keerthanasumathi2004@gmail.com>
2025-04-23 10:33:10 +0530

    azure pipline example

---
+++ src/main/java/mavendemo/proj11a/App.java
@@ -0,0 +1,13 @@
+package mavendemo.proj11a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
